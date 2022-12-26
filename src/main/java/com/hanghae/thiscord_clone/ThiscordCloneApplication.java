82dfc669e1628ef339f7db0f5b0e438de4102d7c
rara-dev <kksr1219@gmail.com>
2022-12-26 20:57:51 +0900

    Initial Commit

---
+++ src/main/java/com/hanghae/thiscord_clone/ThiscordCloneApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.thiscord_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiscordCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiscordCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanghae/thiscord_clone/ThiscordCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.thiscord_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiscordCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
