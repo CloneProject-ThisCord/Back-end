3fba2fd8300e483b2117b7f753c1f356f2abdc23
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2022-12-27 19:15:54 +0900

    feat : 패스워드 인코딩

--- src/main/java/com/hanghae/thiscord_clone/config/WebSecurityConfig.java
+++ src/main/java/com/hanghae/thiscord_clone/config/WebSecurityConfig.java
@@ -1,14 +1,27 @@
 package com.hanghae.thiscord_clone.config;
 
+import com.hanghae.thiscord_clone.security.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final JwtUtil jwtUtil;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
