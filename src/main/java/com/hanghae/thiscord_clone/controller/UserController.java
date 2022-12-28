9de97c355974205a24a98a404ec9df4cadd0f0cc
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2022-12-29 02:38:38 +0900

    fix 로그인시 반환값 변경

--- src/main/java/com/hanghae/thiscord_clone/controller/UserController.java
+++ src/main/java/com/hanghae/thiscord_clone/controller/UserController.java
@@ -6,7 +6,6 @@ import com.hanghae.thiscord_clone.Dto.response.LoginResponseDto;
 import com.hanghae.thiscord_clone.Dto.response.MsgResponseDto;
 import com.hanghae.thiscord_clone.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-@Controller
+
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
